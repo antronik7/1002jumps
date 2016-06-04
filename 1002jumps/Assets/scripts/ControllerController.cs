46cdcbcc092ed07b9944abbb6d096e6531688d13
Antronik <Antronik>
2016-06-04 00:43:41 -0400

    Creation du controller

---
+++ 1002jumps/Assets/scripts/ControllerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerController : MonoBehaviour {
+
+    public GameObject SmallControler;
+
+    private Vector3 initialPos;
+    private Vector3 mousePreviousPos;
+	// Use this for initialization
+	void Start () {
+        initialPos = transform.position;
+        mousePreviousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        SmallControler.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(gameObject);
+        }
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //Vector3 objectPos = mousePos;
+
+        Vector3 objectPos = SmallControler.transform.position + (mousePos - mousePreviousPos);
+
+        objectPos.z = 0f;
+
+
+        Vector3 allowedPos = objectPos - initialPos;
+
+        allowedPos = Vector3.ClampMagnitude(allowedPos, 0.5f);
+
+
+        SmallControler.transform.position = initialPos + allowedPos;
+
+        mousePreviousPos = mousePos;
+    }
+}
---
+++ 1002jumps/Assets/scripts/GameplayController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayController : MonoBehaviour {
+
+    public GameObject controller;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+
+            Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            objectPos.z = 0f;
+
+            Instantiate(controller, objectPos, Quaternion.identity);
+        }
+    }
+}
