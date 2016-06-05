c753a5796a3dbe028ccfb937fd25539209b2e619
antronik7 <antoineberthiaumedutil@gmail.com>
2016-06-05 01:43:45 -0400

    Dash Fonctionnelle

--- 1002jumps/Assets/scripts/ControllerController.cs
+++ 1002jumps/Assets/scripts/ControllerController.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class ControllerController : MonoBehaviour {
 
     public GameObject SmallControler;
-    public GameObject arrow;
 
+    private GameObject player;
+    private GameObject rotator;
+    private GameObject arrow;
     private Vector3 initialPos;
     private Vector3 mousePreviousPos;
-	// Use this for initialization
-	void Start () {
+    private Vector3 difference;
+
+    // Use this for initialization
+    void Start () {
         initialPos = transform.position;
         mousePreviousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         SmallControler.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        player = GameObject.Find("player");
+        rotator = GameObject.Find("player/rotator");
+        arrow = GameObject.Find("player/rotator/arrow");
+        arrow.GetComponent<SpriteRenderer>().enabled = true;
     }
 	
 	// Update is called once per frame
@@ -21,6 +29,8 @@ public class ControllerController : MonoBehaviour {
         
         if (Input.GetMouseButtonUp(0))
         {
+            player.GetComponent<Rigidbody2D>().AddForce(-difference.normalized * arrow.transform.localScale.y * 100);
+            arrow.GetComponent<SpriteRenderer>().enabled = false;
             Destroy(gameObject);
         }
 
@@ -41,5 +51,13 @@ public class ControllerController : MonoBehaviour {
         SmallControler.transform.position = initialPos + allowedPos;
 
         mousePreviousPos = mousePos;
+
+        difference = SmallControler.transform.position - transform.position;
+
+        float rotZ = (Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg) -270;
+
+        rotator.transform.rotation = Quaternion.AngleAxis(rotZ, Vector3.forward);
+
+        arrow.transform.localScale = new Vector3(arrow.transform.localScale.x, 1 + difference.magnitude, arrow.transform.localScale.z);
     }
 }
