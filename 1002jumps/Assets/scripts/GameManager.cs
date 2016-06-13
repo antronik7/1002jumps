97e15465e48e5dce4ec7de3e2d3bf2e51e7644e3
antronik7 <antoineberthiaumedutil@gmail.com>
2016-06-12 20:36:02 -0400

    Ajouter Interface Gold

--- 1002jumps/Assets/scripts/GameManager.cs
+++ 1002jumps/Assets/scripts/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour {
     public static GameManager instance = null;
     public int Score = 0;
     public int HighScore = 0;
+    public int Gold = 0;
+    public bool cinematicStart = false;
+    public GameObject player;
 
     // Use this for initialization
     void Awake () {
@@ -34,7 +37,12 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-	
+        if(cinematicStart)
+        {
+            Debug.Log("Allo");
+            float step = 10 * Time.deltaTime;
+            Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, new Vector3(0, 20, -10), step);
+        }
 	}
 
     public void RestScore()
---
+++ 1002jumps/Assets/scripts/ScoreController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+    Text scoreText;
+	// Use this for initialization
+	void Start () {
+        scoreText = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "x " + GameManager.instance.Gold;
+	}
+}
