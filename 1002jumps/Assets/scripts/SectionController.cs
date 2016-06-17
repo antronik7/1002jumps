361ccf8d69077454984eca5ea35296279a9fcc94
antronik7 <antoineberthiaumedutil@gmail.com>
2016-06-16 23:58:01 -0400

    Coin Spawner

---
+++ 1002jumps/Assets/scripts/SectionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SectionController : MonoBehaviour {
+
+    public GameObject[] coinSpawner;
+    public GameObject coin;
+
+	// Use this for initialization
+	void Start () {
+        if(Random.Range(1, 4) > 3)
+        {
+            int index = Random.Range(0, coinSpawner.Length);
+            Instantiate(coin, coinSpawner[index].transform.position, Quaternion.identity);
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
