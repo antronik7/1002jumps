e82f7c8041205788d4cca9938d044ce5d0e6384d
antronik7 <antoineberthiaumedutil@gmail.com>
2016-06-21 23:05:39 -0400

    Faite la prog pour le score

--- 1002jumps/Assets/scripts/UIController.cs
+++ 1002jumps/Assets/scripts/UIController.cs
@@ -6,15 +6,25 @@ public class UIController : MonoBehaviour {
 
     Text scoreText;
     Text HighScoreText;
+
+    int score;
     // Use this for initialization
     void Start () {
         scoreText = transform.GetChild(0).GetComponent<Text>();
         HighScoreText = transform.GetChild(1).GetComponent<Text>();
+
+        score = GameManager.instance.Score;
     }
 	
 	// Update is called once per frame
 	void Update () {
         scoreText.text = GameManager.instance.Score.ToString();
         HighScoreText.text = GameManager.instance.HighScore.ToString();
+
+        if(GameManager.instance.Score > score)
+        {
+            transform.GetChild(0).GetComponent<Animator>().SetTrigger("GetPoint");
+            score = GameManager.instance.Score;
+        }
     }
 }
