1506c54207cc2836cb8cfcd9afaa623e0bb4faad
antronik7 <antoineberthiaumedutil@gmail.com>
2016-06-24 23:58:09 -0400

    Classe meteors spawner

---
+++ 1002jumps/Assets/scripts/MeteorLauncher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorLauncher : MonoBehaviour {
+
+    public GameObject[] meteors;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("SpawnMeteor", 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnMeteor()
+    {
+        float randomTime = Random.Range(0.5f, 2);
+        int randomIndex = Random.Range(0, meteors.Length);
+
+        Instantiate(meteors[randomIndex]);
+
+        Invoke("SpawnMeteor", randomTime);
+    }
+}
