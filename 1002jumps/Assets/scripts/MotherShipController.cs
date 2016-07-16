f080416c5fd95696deb49fae88acc25d08615cd1
antronik7 <antoineberthiaumedutil@gmail.com>
2016-07-16 00:04:49 -0400

    Vraiment fini mothership

---
+++ 1002jumps/Assets/scripts/MotherShipController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherShipController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.gameStart)
+        {
+            transform.Translate(Vector3.down * (Time.deltaTime * (2.5f + GameManager.instance.Score / 10)));
+        }
+    }
+}
