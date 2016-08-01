48accf076ed49d49ef2293ba4c3e5d067919c42a
antronik7 <antoineberthiaumedutil@gmail.com>
2016-07-31 23:58:46 -0400

    Commencer le reflect sur le mur

---
+++ 1002jumps/Assets/scripts/ReflectWall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision collision)
+    {
+        if(collision.gameObject.tag == "player")
+        {
+            collision.rigidbody.velocity = Vector3.Reflect(collision.rigidbody.velocity, Vector2.left);
+        }
+
+        //rigidbody.velocity = Vector3.Reflect(rigidbody.velocity, collision.contacts[0].normal);
+    }
+}
