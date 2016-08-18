2ee96972ad76f1614a132e2c91dce3590bb4a1b5
antronik7 <antoineberthiaumedutil@gmail.com>
2016-08-18 00:14:02 -0400

    Creer la section avec les meteorite qui spawn

---
+++ 1002jumps/Assets/test1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test1 : MonoBehaviour {
+
+    public int lol;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(lol, 0) * 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
