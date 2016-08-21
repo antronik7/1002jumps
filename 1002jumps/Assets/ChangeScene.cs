7f232e271af271c82a74168922145e88af12a927
antronik7 <antoineberthiaumedutil@gmail.com>
2016-08-21 03:27:34 -0400

    version web

---
+++ 1002jumps/Assets/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string nomScene;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changerDeScene()
+    {
+        Application.LoadLevel(nomScene);
+    }
+}
